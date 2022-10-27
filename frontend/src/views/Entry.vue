b830b03649ecdab6e1d7ba89fd41559b6661df08
F-Node-Karlsruhe <christian.fries@eecc.de>
2022-10-27 15:04:26 +0200

    fix logo

--- frontend/src/views/Entry.vue
+++ frontend/src/views/Entry.vue
@@ -2,7 +2,7 @@
     <div class="card m-auto shadow" style="min-width: 80%">
         <div class="card-header text-center p-3">
             <h3>Credential Selector</h3>
-            <a href="https://eecc.info"><img id="logo" src="./logo.png"/></a>
+            <a href="https://eecc.info"><img id="logo" src="@/assets/img/logo.png"/></a>
         </div>
         <div class="card-body m-3">
             <p>Select the credential/s using one of the options below.</p>
