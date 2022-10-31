d185ddadf495614d99ef9af9b490bb8816a1004f
F-Node-Karlsruhe <christian.fries@eecc.de>
2022-10-31 10:37:07 +0100

    fix warning

--- frontend/src/views/QRModal.vue
+++ frontend/src/views/QRModal.vue
@@ -7,7 +7,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-center">
-                    <qrcode-vue :value="JSON.stringify(value)" margin="1" :size="300" level="M" class="m-3" :id="'canvas-' + id"/>
+                    <qrcode-vue :value="JSON.stringify(value)" :margin="1" :size="300" level="M" class="m-3" :id="'canvas-' + id"/>
                 </div>
                 <div class="modal-footer justify-content-center">
                     <button type="button" class="btn btn-outline-primary" @click="downloadQR()"><i class="bi-download" role="img" aria-label="QR Download"></i> Download QR-Code</button>
