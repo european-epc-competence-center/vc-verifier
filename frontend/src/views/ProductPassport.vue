013531e77fda69c3885ef8bb63396f7ef680851f
F-Node-Karlsruhe <christian.fries@eecc.de>
2022-10-31 11:02:48 +0100

    improve product passport

--- frontend/src/views/ProductPassport.vue
+++ frontend/src/views/ProductPassport.vue
@@ -3,28 +3,80 @@
         <div class="card-header text-success p-3">
             <h5>Verified Product Passport</h5>
         </div>
-        <div class="row m-3">
+        <div class="card-body p-3">
+        <div class="row mb-3">
             <div class="col-md-8">
-                <h5>{{properties.product}}</h5>
+                <h3 class="ms-3 mb-3">{{properties.product}}</h3>
+                <ul class="list-group">
+                    <Transition name="list">
+                        <li v-if="properties.production_date" class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <strong>Brand</strong>
+                                </div>
+                                <div class="col-md-6">
+                                    {{properties.brand}}
+                                </div>
+                            </div>
+                        </li>
+                    </Transition>
+                    <Transition name="list">
+                        <li v-if="properties.production_date" class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <strong>Model</strong>
+                                </div>
+                                <div class="col-md-6">
+                                    {{properties.model}}
+                                </div>
+                            </div>
+                        </li>
+                    </Transition>
+                    <Transition name="list">
+                        <li v-if="properties.production_date" class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <strong>Production date</strong>
+                                </div>
+                                <div class="col-md-6">
+                                    {{properties.production_date}}
+                                </div>
+                            </div>
+                        </li>
+                    </Transition>
+                </ul>
             </div>
-            <div class="col-md-4 pe-0">
-                <img class="rounded" id="productpassportimg" :src="properties.product_img" :alt="properties.product_img"/>
+            <div class="col-md-4">
+                <Transition name="slide-fade">
+                    <img v-if="properties.product_img" class="rounded" id="productpassportimg" :src="properties.product_img" :alt="properties.product_img"/>
+                </Transition>
             </div>
         </div>
-        <div class="card-body p-3">
-            <h5 class="ms-3">Details</h5>
-            <TransitionGroup name="list" tag="ul" class="list-group">
-                <li v-for="(value, key) in properties" :key="key" class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <strong>{{key}}</strong>
-                        </div>
-                        <div class="col-md-6">
-                            {{value}}
-                        </div>
+            <div class="accordion" id="productpassportacc">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="productpassportacchead">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#productpassportdetails" aria-expanded="false" aria-controls="productpassportdetails">
+                        Details
+                    </button>
+                    </h2>
+                    <div id="productpassportdetails" class="accordion-collapse collapse" aria-labelledby="productpassportacchead" data-bs-parent="productpassportacc">
+                    <div class="accordion-body">
+                        <TransitionGroup name="list" tag="ul" class="list-group">
+                            <li v-for="(value, key) in properties" :key="key" class="list-group-item">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <strong>{{key}}</strong>
+                                    </div>
+                                    <div class="col-md-6">
+                                        {{value}}
+                                    </div>
+                                </div>
+                            </li>
+                        </TransitionGroup>
                     </div>
-                </li>
-            </TransitionGroup>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
