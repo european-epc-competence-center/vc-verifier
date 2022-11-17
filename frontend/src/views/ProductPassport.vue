b1cf0c48bb028f16fdd41320ac65f05d9ee9028c
F-Node-Karlsruhe <christian.fries@eecc.de>
2022-11-17 14:37:53 +0100

    make product passort main props more generic

--- frontend/src/views/ProductPassport.vue
+++ frontend/src/views/ProductPassport.vue
@@ -6,45 +6,19 @@
         <div class="card-body p-3">
         <div class="row mb-3">
             <div class="col-md-8">
-                <h3 class="ms-3 mb-3">{{properties.product}}</h3>
-                <ul class="list-group">
-                    <Transition name="list">
-                        <li v-if="properties.brand" class="list-group-item">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <strong>Brand</strong>
-                                </div>
-                                <div class="col-md-6">
-                                    {{properties.brand}}
-                                </div>
+                <h3 v-if="properties.product" class="ms-3 mb-3">{{properties.product}}</h3>
+                <TransitionGroup name="list" tag="ul" class="list-group">
+                    <li v-for="(value, key) in getMainProps" :key="key" class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <strong>{{mainProps[key]}}</strong>
                             </div>
-                        </li>
-                    </Transition>
-                    <Transition name="list">
-                        <li v-if="properties.model" class="list-group-item">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <strong>Model</strong>
-                                </div>
-                                <div class="col-md-6">
-                                    {{properties.model}}
-                                </div>
+                            <div class="col-md-6">
+                                {{value}}
                             </div>
-                        </li>
-                    </Transition>
-                    <Transition name="list">
-                        <li v-if="properties.production_date" class="list-group-item">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <strong>Production date</strong>
-                                </div>
-                                <div class="col-md-6">
-                                    {{properties.production_date}}
-                                </div>
-                            </div>
-                        </li>
-                    </Transition>
-                </ul>
+                        </div>
+                    </li>
+                </TransitionGroup>
             </div>
             <div class="col-md-4 mt-md-0 mt-3">
                 <Transition name="slide-fade">
@@ -89,8 +63,24 @@ export default {
     },
     data() {
         return {
-            
+            mainProps: {
+                'brand': 'Brand',
+                'model': 'Model',
+                'clockworkType': 'Clockwork',
+                'caseMaterial': 'Case Material'
+            }
         }
     },
+    computed: {
+        getMainProps() {
+            return Object.keys(this.mainProps)
+                            .filter((key) => Object.keys(this.properties).includes(key))
+                            .reduce((obj, key) => {
+                                return Object.assign(obj, {
+                                [key]: this.properties[key]
+                                });
+                            }, {});
+        }
+    }
 }
 </script>
