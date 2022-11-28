20f6bc088b0375ba7ce65320ac0be5ec9a6823f9
F-Node-Karlsruhe <christian.fries@eecc.de>
2022-11-28 16:36:03 +0100

    allow cors for dev

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,9 @@ const axiosInstance =  axios.create({
   baseURL: process.env.VERIFIER_API || 'https://ssi.eecc.de/api/verifier',
   timeout: 5000,
   headers: {
-    'Accept': 'application/json',
+    'Accept': 'application/ld+json,application/json,*/*',
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
   },
 });
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.PUBLIC_PATH + '/'
-    : '/'
+    : '/',
+  configureWebpack: {
+      devServer: {
+        headers: { 'Access-Control-Allow-Origin': '*' }
+      }
+    }
 })
