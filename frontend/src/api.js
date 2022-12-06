749e5d8aa86bc0d10abaa6c33122c10998dc8477
F-Node-Karlsruhe <christian.fries@eecc.de>
2022-12-06 11:18:56 +0100

    fix cors issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,9 +3,7 @@ const axiosInstance =  axios.create({
   baseURL: process.env.VERIFIER_API || 'https://ssi.eecc.de/api/verifier',
   timeout: 5000,
   headers: {
-    'Accept': 'application/ld+json,application/json,*/*',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Accept': 'application/ld+json,application/json,*/*'
   },
 });
 
