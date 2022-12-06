7ecc81c6bf3c3cecaefc24c0c10378e57458059d
F-Node-Karlsruhe <christian.fries@eecc.de>
2022-12-06 11:42:37 +0100

    add verison tag

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="d-flex justify-content-center h-100 m-1">
       <router-view />
+      <div class="versiontag text-light m-1">Version {{this.$store.state.version}}</div>
     </div>
 </template>
 <script>
