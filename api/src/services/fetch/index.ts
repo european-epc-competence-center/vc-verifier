8bc43d4373f7a4ac7a9076521caf8a159d7a8757
F-Node-Karlsruhe <christian.fries@eecc.de>
2023-01-24 14:25:07 +0100

    improve content type recognition

--- api/src/services/fetch/index.ts
+++ api/src/services/fetch/index.ts
@@ -13,21 +13,27 @@ export async function fetch_jsonld(url: string): Promise<object> {
 
     const response = await fetch(url, { method: 'GET', headers: JSONLD_HEADERS});
 
-    // no json-ld returned -> search for json-ld link
-    if (response.headers.get('content-type') != 'application/ld+json') {
+    const contentType = response.headers.get("content-type");
 
-        const link = parseLink(response.headers.get('Link'));
+    // return if proper json-ld is returned
+    if (contentType && contentType.indexOf('application/ld+json') != -1) {
 
-        if (link && link.alternate && link.alternate.rel == 'alternate' && link.alternate.type == 'application/ld+json') {
-
-            const linkResponse = await fetch(url + link.alternate.url, { method: 'GET', headers: JSONLD_HEADERS});
-
-            return await linkResponse.json();
-
-        }
+        return await response.json();
 
     }
 
+    // search for json-ld link if no json-ld is returned
+    const link = parseLink(response.headers.get('Link'));
+
+    if (link && link.alternate && link.alternate.rel == 'alternate' && link.alternate.type == 'application/ld+json') {
+
+        const linkResponse = await fetch(url + link.alternate.url, { method: 'GET', headers: JSONLD_HEADERS});
+
+        return await linkResponse.json();
+
+    }
+
+    // try fetching json in any case
     return await response.json();
     
 }
