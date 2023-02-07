75bdf41cfd47d14591721687f12fa3774b729fcf
F-Node-Karlsruhe <christian.fries@eecc.de>
2023-02-07 12:08:50 +0100

    enable presentations in frontend

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,7 @@
+export const VerifiableType = {
+    CREDENTIAL: 'VerifiableCredential',
+    PRESENTATION: 'VerifiablePresentation'
+};
 
 export function isURL(url) {
     if (typeof url != 'string') return false;
@@ -9,9 +13,15 @@ export function getCredentialValue(value) {
 }
 
 export function getPlainCredential(credential) {
-    var clean_credential = {...credential};
+    var clean_credential = { ...credential };
     delete clean_credential.verified;
     delete clean_credential.revoked;
     delete clean_credential.status;
+    delete clean_credential.presentation;
     return clean_credential;
+}
+
+export function getVerifiableType(verifiable) {
+    if (verifiable.type.includes(VerifiableType.PRESENTATION)) return VerifiableType.PRESENTATION;
+    return VerifiableType.CREDENTIAL;
 }
