5c8d9c5ec203a13ac2e4c823d3fe70cd2147d84b
F-Node-Karlsruhe <christian.fries@eecc.de>
2023-06-02 13:09:31 +0200

    fix qr overlfow error / 8
    Signed-off-by: F-Node-Karlsruhe <christian.fries@eecc.de>

--- frontend/src/components/QRModal.vue
+++ frontend/src/components/QRModal.vue
@@ -3,15 +3,17 @@
         <div class="modal-dialog modal-dialog-centered modal-fullscreen-md-down">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5 text-primary" id="exampleModalToggleLabel">{{value.type[1]}}</h1>
+                    <h1 class="modal-title fs-5 text-primary" id="exampleModalToggleLabel">{{ value.type[1] }}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-center">
-                    <qrcode-vue v-if="JSON.stringify(value).length < 18672" :value="JSON.stringify(value)" :margin="1" :size="300" level="M" class="m-3" :id="'canvas-' + id"/>
+                    <qrcode-vue v-if="JSON.stringify(value).length < 2334" :value="JSON.stringify(value)" :margin="1"
+                        :size="300" level="M" class="m-3" :id="'canvas-' + id" />
                     <h5 v-else class="text-danger">Credential too large for display!</h5>
                 </div>
                 <div class="modal-footer justify-content-center">
-                    <button type="button" class="btn btn-outline-primary" @click="downloadQR()"><i class="bi-download" role="img" aria-label="QR Download"></i> Download QR-Code</button>
+                    <button type="button" class="btn btn-outline-primary" @click="downloadQR()"><i class="bi-download"
+                            role="img" aria-label="QR Download"></i> Download QR-Code</button>
                 </div>
             </div>
         </div>
@@ -32,7 +34,7 @@ export default {
     },
     data() {
         return {
-            
+
         }
     },
     methods: {
