5ffcc872bee63d732ef1da2fb19521c2f2601227
F-Node-Karlsruhe <christian.fries@eecc.de>
2024-04-23 15:45:47 +0200

    mv to openid jwt verifier
    Signed-off-by: F-Node-Karlsruhe <christian.fries@eecc.de>

--- api/src/services/verifier/sdjwt.ts
+++ api/src/services/verifier/sdjwt.ts
@@ -1,36 +1,89 @@
 import { SDJwtVcInstance } from "@sd-jwt/sd-jwt-vc";
-import type { DisclosureFrame } from "@sd-jwt/types";
-import { ES256, digest, generateSalt } from "@sd-jwt/crypto-nodejs";
+import type { Verifier, KbVerifier } from "@sd-jwt/types";
+import { ES256, digest } from "@sd-jwt/crypto-nodejs";
+import { importJWK, JWK, JWTPayload, jwtVerify } from "jose";
+import { dereferenceDID } from "../documentLoader/index.js";
+import { fetch_json, isURL } from "../fetch/index.js";
 
-const createSignerVerifier = async () => {
-  const { privateKey, publicKey } = await ES256.generateKeyPair();
-  return {
-    signer: await ES256.getSigner(privateKey),
-    verifier: await ES256.getVerifier(publicKey),
-  };
-};
-
-const { signer, verifier } = await createSignerVerifier();
+async function getPublicKey(issuer: string, kid: string): Promise<JWK> {
+  if (isURL(issuer)) {
+    const response = await fetch_json(`${issuer}/.well-known/jwt-vc-issuer`)
+      .then((r: any) => r.data)
+      .catch(() => {
+        throw new Error("Error on fetching public key from " + issuer);
+      });
+    const key = response.jwks.keys.find((key: any) => key.kid === kid);
+    if (!key) {
+      throw new Error("Key not found und well-known");
+    }
+    return key;
+  }
+  if (kid && kid.startsWith("did:")) {
+    const absoluteDidUrl =
+      kid && kid.startsWith(issuer) ? kid : `${issuer}#${kid}`;
+    const { publicKeyJwk } = (await dereferenceDID(absoluteDidUrl))?.document;
+    if (!publicKeyJwk) {
+      throw new Error("Key not found in did");
+    }
+    return publicKeyJwk;
+  }
+  throw new Error("Could not resolve public keys");
+}
 
 export async function verifySDJWT(
   verifiable: string,
   nonce?: string,
   aud?: string
 ): Promise<VerificationResult> {
-  const sdjwt = new SDJwtVcInstance({
-    signer,
-    verifier,
-    signAlg: "EdDSA",
-    hasher: digest,
-    hashAlg: "SHA-256",
-    saltGenerator: generateSalt,
-  });
+  try {
+    let sdjwtInstance: SDJwtVcInstance;
+    /**
+     * The verifier function. This function will verify the signature of the vc.
+     * @param data encoded header and payload of the jwt
+     * @param signature signature of the jwt
+     * @returns true if the signature is valid
+     */
+    const verifier: Verifier = async (data, signature) => {
+      const decodedVC = await sdjwtInstance.decode(`${data}.${signature}`);
+      const payload = decodedVC.jwt?.payload as JWTPayload;
+      const header = decodedVC.jwt?.header as JWK;
+      const publicKey = await getPublicKey(
+        payload.iss as string,
+        header.kid as string
+      );
+      const verify = await ES256.getVerifier(publicKey);
+      return verify(data, signature);
+    };
 
-  const decodedObject = await sdjwt.decode(verifiable);
+    /**
+     * The kb verifier function. This function will verify the signature for the key binding
+     * @param data
+     * @param signature
+     * @param payload
+     * @returns
+     */
+    const kbVerifier: KbVerifier = async (data, signature, payload) => {
+      if (!payload.cnf) {
+        throw new Error("No cnf found in the payload");
+      }
+      const key = await importJWK(payload.cnf.jwk as JWK, "ES256");
+      return jwtVerify(`${data}.${signature}`, key).then(
+        () => true,
+        () => false
+      );
+    };
 
-  const verified = await sdjwt.verify(verifiable, []);
-
-  console.log(verified);
-
-  return { verified: true };
+    // initialize the sdjwt instance.
+    sdjwtInstance = new SDJwtVcInstance({
+      hasher: digest,
+      verifier,
+      kbVerifier,
+    });
+    // verify the presentation.
+    await sdjwtInstance.verify(verifiable, [], true);
+    return Promise.resolve({ verified: true });
+  } catch (e) {
+    console.error(e);
+    return Promise.reject({ verified: false, error: (e as Error).message });
+  }
 }
